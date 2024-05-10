6b4bacc59d5fe6b21c654c45c01fbdb8688ee8b0
Deveshi Dwivedi <120312681+deveshidwivedi@users.noreply.github.com>
2024-05-10 23:10:44 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,12 +12,12 @@ import { Landing } from "./components/Landing";
 import { Submissions } from "./components/Sumissions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjjsbl9eSDWSmfrWpFPap2uGuwONZ2N4g",
+  apiKey: "",
   authDomain: "leetcode-clone-c39eb.firebaseapp.com",
   projectId: "leetcode-clone-c39eb",
   storageBucket: "leetcode-clone-c39eb.appspot.com",
-  messagingSenderId: "66814187798",
-  appId: "1:66814187798:web:a6b3702e191448722dd837",
+  messagingSenderId: "",
+  appId: "",
   measurementId: "G-ET5FNB5WCN"
 };
 

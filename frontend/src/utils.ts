9b0d68955eff79215a4581f57c42a7b1db858b16
Deveshi Dwivedi <120312681+deveshidwivedi@users.noreply.github.com>
2024-05-10 23:10:17 +0530

    Update utils.ts

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjjsbl9eSDWSmfrWpFPap2uGuwONZ4N4g",
+  apiKey: "",
   authDomain: "leetcode-clone-c39eb.firebaseapp.com",
   projectId: "leetcode-clone-c39eb",
   storageBucket: "leetcode-clone-c39eb.appspot.com",
-  messagingSenderId: "66814187798",
+  messagingSenderId: "",
   appId: "1:66814187798:web:a6b3702e191448722df837",
   measurementId: "G-ET5FNB5WCN"
 };
